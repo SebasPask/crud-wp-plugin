bc3d1f89aac30afa56c1bf52a2f1733bf63c98db
Joan Sebastian Pascagaza Guerrero <sebaspask@gmail.com>
2018-05-25 08:49:25 +1000

    initial commit

---
+++ beziercode-blank/admin/class-bc-admin.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * La funcionalidad específica de administración del plugin.
+ *
+ * @link       http://misitioweb.com
+ * @since      1.0.0
+ *
+ * @package    Beziercode_blank
+ * @subpackage Beziercode_blank/admin
+ */
+
+/**
+ * Define el nombre del plugin, la versión y dos métodos para
+ * Encolar la hoja de estilos específica de administración y JavaScript.
+ * 
+ * @since      1.0.0
+ * @package    Beziercode-Blank
+ * @subpackage Beziercode-Blank/admin
+ * @author     Gilbert Rodríguez <email@example.com>
+ * 
+ * @property string $plugin_name
+ * @property string $version
+ */
+class BC_Admin {
+    
+    /**
+	 * El identificador único de éste plugin
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name  El nombre o identificador único de éste plugin
+	 */
+    private $plugin_name;
+    
+    /**
+	 * Versión actual del plugin
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version  La versión actual del plugin
+	 */
+    private $version;
+    
+    /**
+     * @param string $plugin_name nombre o identificador único de éste plugin.
+     * @param string $version La versión actual del plugin.
+     */
+    public function __construct( $plugin_name, $version ) {
+        
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;     
+        
+    }
+    
+    /**
+	 * Registra los archivos de hojas de estilos del área de administración
+	 *
+	 * @since    1.0.0
+     * @access   public
+	 */
+    public function enqueue_styles() {
+        
+        /**
+         * Una instancia de esta clase debe pasar a la función run()
+         * definido en BC_Cargador como todos los ganchos se definen
+         * en esa clase particular.
+         *
+         * El BC_Cargador creará la relación
+         * entre los ganchos definidos y las funciones definidas en este
+         * clase.
+		 */
+        
+        
+        wp_enqueue_style( 'bc_material_icons',  'https://fonts.googleapis.com/icon?family=Material+Icons', array(), $this->version, 'all' );
+        
+        wp_enqueue_style( 'bc_materialize_admin_css', BC_PLUGIN_DIR_URL . 'helpers/materialize/css/materialize.min.css', array(), $this->version, 'all' );
+
+
+        wp_enqueue_style( 'bc_swwet_alert_css', BC_PLUGIN_DIR_URL . '/helpers/sweetalert/sweetalert.css', array(), $this->version, 'all' );
+
+        wp_enqueue_style( $this->plugin_name, BC_PLUGIN_DIR_URL . 'admin/css/bc-admin.css', array(), $this->version, 'all' );
+
+    }
+    
+    /**
+	 * Registra los archivos Javascript del área de administración
+	 *
+	 * @since    1.0.0
+     * @access   public
+	 */
+    public function enqueue_scripts() {
+        
+        /**
+         * Una instancia de esta clase debe pasar a la función run()
+         * definido en BC_Cargador como todos los ganchos se definen
+         * en esa clase particular.
+         *
+         * El BC_Cargador creará la relación
+         * entre los ganchos definidos y las funciones definidas en este
+         * clase.
+		 */
+        wp_enqueue_script( 'bc_materialize_admin_js', BC_PLUGIN_DIR_URL . 'helpers/materialize/materialize.js', ['jquery'], $this->version, true );
+
+        wp_enqueue_script( 'bc_swwet_alert_js', BC_PLUGIN_DIR_URL .  '/helpers/sweetalert/sweetalert.min.js', ['jquery'], $this->version, true );
+
+        wp_enqueue_script( $this->plugin_name, BC_PLUGIN_DIR_URL . 'admin/js/bc-admin.js', ['jquery'], $this->version, true );
+
+        
+    }
+    
+}
+
+
+
+
+
+
+
